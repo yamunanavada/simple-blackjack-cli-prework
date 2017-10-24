f5bc1a388547315de8fa1a3232efe3d170d7c6d8
Yamuna Navada <yamunanavada@gmail.com>
2017-10-24 14:38:37 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,8 +8,9 @@ def deal_card
   rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  # code #display_card_total here'
+  puts total
 end
 
 def prompt_user
